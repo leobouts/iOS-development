9f991a7462f6b17fccc3f2bea36a40cf2bf4703a
leobouts <leobouts@gmail.com>
2017-07-30 23:39:35 +0300

    notification practise

---
+++ NewNotifications/NewNotifications/ViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  NewNotifications
+//
+//  Created by Leonidas on 30/07/2017.
+//  Copyright © 2017 Leonidas. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 1. REQUEST PERMISSION
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { (granted, error) in
+            if granted {
+                self.loadNotification()
+            } else {
+                print(error?.localizedDescription)
+            }
+        })
+    }
+    
+    // 3. SCHEDULE NOTIFICATION
+    @IBAction func notifyMeButtonTapped(sender: UIButton) {
+        scheduleNotification(inSeconds: 5, completion: { success in
+            if success {
+                print("Successfully scheduled notification")
+            } else {
+                print("Error scheduling notification")
+            }
+        })
+    }
+    
+    func loadNotification() {
+        
+    }
+    
+    // 2. CREATE NOTIFICATION
+    func scheduleNotification(inSeconds: TimeInterval, completion: @escaping (_ Success: Bool) -> ()) {
+        
+        // Add an attachment
+        let myImage = "rick_grimes"
+        guard let imageUrl = Bundle.main.url(forResource: myImage, withExtension: "gif") else {
+            completion(false)
+            return
+        }
+        
+        var attachment: UNNotificationAttachment
+        
+        attachment = try! UNNotificationAttachment(identifier: "myNotification", url: imageUrl, options: .none)
+        
+        // Create our notification content
+        let notif = UNMutableNotificationContent()
+        
+        // ONLY FOR EXTENSION
+        notif.categoryIdentifier = "myNotificationCategory"
+        
+        notif.title = "New Notification!"
+        notif.subtitle = "These are great!"
+        notif.body = "The new notification options in iOS 10 are what I've been waiting for! 👊"
+        
+        // Add our attachment
+        notif.attachments = [attachment]
+        
+        let notifTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "myNotification", content: notif, trigger: notifTrigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+            if error != nil {
+                print(error)
+                completion(false)
+            } else {
+                completion(true)
+            }
+        })
+    }
+}
+
